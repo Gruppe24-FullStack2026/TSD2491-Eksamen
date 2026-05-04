b78d02c25c7690889c450a0e3726e306b527a97c
Gruppe24-1 <Gruppe24-1@example.com>
2026-05-04 17:17:14 +0200

    Krav 3: Opprettet DbContext for å koble DbContext i Program.cs og opprettet database via EF Core

---
+++ BookApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using BookApp.Models;
+
+namespace BookApp.Data;
+
+/// <summary>
+/// Database context for the BookApp application.
+/// </summary>
+public class ApplicationDbContext : DbContext
+{
+    /// <summary>
+    /// Initializes a new instance of ApplicationDbContext.
+    /// </summary>
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    /// <summary>
+    /// Database table for books.
+    /// </summary>
+    public DbSet<Book> Books { get; set; }
+
+    /// <summary>
+    /// Database table for authors.
+    /// </summary>
+    public DbSet<Author> Authors { get; set; }
+
+    /// <summary>
+    /// Database table for libraries.
+    /// </summary>
+    public DbSet<Library> Libraries { get; set; }
+}
--- BookApp/Program.cs
+++ BookApp/Program.cs
@@ -1,7 +1,11 @@
+using BookApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
